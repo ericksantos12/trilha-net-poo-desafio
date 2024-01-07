88f2b035dda1f74c1d43c062a27096fa1abb3c4a
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2024-01-07 13:36:48 -0300

    implement inheritance and override methods in Iphone and Nokia classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+Iphone iphone1 = new Iphone("987654321", "iPhone 11", "987654321", 64);
+
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Colossatron");
+
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Minecraft");
